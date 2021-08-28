3b5c424c29af13cf67a1adca4b5e230b02b91ef7
makkesann <makkesann@gmail.com>
2021-08-28 16:21:10 +0900

    go module10

--- server/controller/index.go
+++ server/controller/index.go
@@ -1,9 +1,11 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func IndexDisplayAction(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"massage": "ping",
 	})
-},
+}
